acfec7dd8cd822859d95aba1e84992ffb818bc82
sajjang <grahith.sajjanapu@dominos.com>
2018-09-12 09:49:16 -0400

    Initial commit

---
+++ src/main/java/com/grahith/csvparsing/CsvParsingApplication.java
@@ -0,0 +1,12 @@
+package com.grahith.csvparsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvParsingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvParsingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/grahith/csvparsing/CsvParsingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grahith.csvparsing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CsvParsingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
